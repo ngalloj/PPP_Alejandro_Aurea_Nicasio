af817ec8f8dfa13d07ba4a01cce20c25f4f55b73
ngalloj <nicasiomanuelgalindolojo@alumno.ieselrincon.es>
2026-02-19 21:37:50 +0000

    Cambios en db.config.js

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -3,11 +3,17 @@ require('dotenv').config();
 
 //se asigna valores a los parametros de la base de datos. 
 module.exports = {
-  HOST: process.env.DB_HOST,
+  /* HOST: process.env.DB_HOST,
   USER: process.env.MYSQL_USER,
   PASSWORD: process.env.MYSQL_PASSWORD,
   DB:  process.env.MYSQL_DATABASE,
-  dialect: "mysql",
+  dialect: "mysql", */
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
+    PORT: process.env.DB_PORT,
+    dialect: "mysql",
   pool: {
     max: 25,
     min: 0,
